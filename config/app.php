64484c25d98614fed3e47726e5e8d968b78a0dd5
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2023-11-11 15:35:52 +0630

    code format style with pint

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
          * Package Service Providers...
          */
 
-         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
