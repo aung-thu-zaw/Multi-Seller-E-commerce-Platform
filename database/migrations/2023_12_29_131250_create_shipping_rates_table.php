02c88b2808f21ce816b035b723d94f7ab794b2d6
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2023-12-29 21:54:09 +0630

    create tables for shipping feature

---
+++ database/migrations/2023_12_29_131250_create_shipping_rates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipping_rates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("shipping_area_id")->constrained();
+            $table->foreignId("shipping_method_id")->constrained();
+            $table->decimal('min_order_total', 8, 2)->nullable();
+            $table->decimal('max_order_total', 8, 2)->nullable();
+            $table->decimal("rate", 8, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipping_rates');
+    }
+};
