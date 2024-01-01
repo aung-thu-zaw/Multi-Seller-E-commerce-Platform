982dbec9e7aea6f94178a51de2b98cbda5ae8b53
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-01-01 20:28:59 +0630

    fixed paypal payment bug and create order with paypal payment

--- app/Http/Controllers/Ecommerce/Payments/PaypalController.php
+++ app/Http/Controllers/Ecommerce/Payments/PaypalController.php
@@ -3,42 +3,42 @@
 namespace App\Http\Controllers\Ecommerce\Payments;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\CheckoutPayment;
+use App\Mail\Admin\NewOrderPlacedEmail;
+use App\Mail\Seller\NewOrderPlacedEmail as SellerNewOrderPlacedEmail;
+use App\Mail\User\OrderPlacedSuccessfullyEmail;
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\ShippingMethod;
+use App\Models\Store;
+use App\Models\Transaction;
+use App\Models\User;
 use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 class PaypalController extends Controller
 {
-    public function config(): array
-    {
-        return [
-            'mode' => env('PAYPAL_MODE', 'sandbox'),
-            'sandbox' => [
-                'client_id' => env('PAYPAL_SANDBOX_CLIENT_ID', ''),
-                'client_secret' => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
-                'app_id' => 'APP-80W284485P519543T',
-            ],
-            'live' => [
-                'client_id' => env('PAYPAL_LIVE_CLIENT_ID', ''),
-                'client_secret' => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
-                'app_id' => env('PAYPAL_LIVE_APP_ID', ''),
-            ],
+    use CheckoutPayment;
 
-            'payment_action' => env('PAYPAL_PAYMENT_ACTION', 'Sale'),
-            'currency' => env('PAYPAL_CURRENCY', 'USD'),
-            'notify_url' => env('PAYPAL_NOTIFY_URL', ''),
-            'locale' => env('PAYPAL_LOCALE', 'en_US'),
-            'validate_ssl' => env('PAYPAL_VALIDATE_SSL', true),
-        ];
-    }
-
-    public function payWithPaypal()
+    public function payWithPaypal(): RedirectResponse
     {
+        $coupon = session('coupon') ?? null;
+        $cartItems = auth()->user()->cart->cartItems;
+        $address = Address::where('user_id', auth()->id())->where('is_default_delivery', 1)->first();
+        $shippingArea = $this->getShippingArea($address);
+        $totalCartItemAmount = $this->calculateTotalCartItemAmount($cartItems, $coupon);
+        $shippingRate = $this->getShippingRate($shippingArea, $cartItems, $totalCartItemAmount);
+
         try {
-            $config = $this->config();
+            $provider = new PayPalClient();
 
-            $provider = new PayPalClient($config);
+            $provider->setApiCredentials(config('paypal'));
 
             $provider->getAccessToken();
 
@@ -51,50 +51,131 @@ class PaypalController extends Controller
                 'purchase_units' => [
                     [
                         'amount' => [
-                            'currency_code' => $config['currency'],
-                            'value' => 250.89,
-                            // "value" => $request->price
+                            'currency_code' => 'USD',
+                            'value' =>  $totalCartItemAmount + $shippingRate->rate,
                         ],
                     ],
                 ],
             ]);
 
             if (isset($response['id']) && $response['id'] != null) {
-                foreach ($response['links'] as $link) {
-                    if ($link['rel'] === 'approve') {
-                        // dd($link['href']);
-                        return redirect()->away($link['href']);
+                foreach ($response['links'] as $links) {
+                    if ($links['rel'] === 'approve') {
+                        return redirect()->away($links['href']);
                     }
                 }
             } else {
+
                 return redirect()->route('payments.paypal.cancel');
             }
         } catch (Exception $e) {
             Log::error('PayPal API Error: '.$e->getMessage());
 
-            return response()->json(['error' => 'Internal Server Error'], 500);
+            return back()->with("error", "Something went wrong!");
         }
     }
 
-    public function paypalSuccess(Request $request)
+    public function paypalSuccess(Request $request): RedirectResponse
     {
-        $config = $this->config();
+        $coupon = session('coupon') ?? null;
+        $cartItems = auth()->user()->cart->cartItems;
+        $shippingMethod = ShippingMethod::find($cartItems[0]->shipping_method_id);
+        $address = Address::where('user_id', auth()->id())->where('is_default_delivery', 1)->first();
+        $shippingArea = $this->getShippingArea($address);
+        $totalCartItemAmount = $this->calculateTotalCartItemAmount($cartItems, $coupon);
+        $shippingRate = $this->getShippingRate($shippingArea, $cartItems, $totalCartItemAmount);
 
-        $provider = new PayPalClient($config);
+        $productsByStore = [];
 
-        $provider->getAccessToken();
+        try {
 
-        $response = $provider->capturePaymentOrder($request->token);
+            $provider = new PayPalClient(config('paypal'));
 
-        if (isset($response['status']) && $response['status'] === 'COMPLETED') {
-            dd('Paid Successfully');
+            $provider->getAccessToken();
+
+            $response = $provider->capturePaymentOrder($request->token);
+
+            if (isset($response['status']) && $response['status'] === 'COMPLETED') {
+
+                DB::transaction(function () use ($totalCartItemAmount, $shippingRate, $cartItems, $shippingMethod, $address, &$productsByStore) {
+                    $order = Order::create([
+                        'user_id' => auth()->id(),
+                        'invoice_no' => 'E-COMMERCE' . mt_rand(100000000, 999999999),
+                        'tracking_no' => '#' . uniqid(),
+                        'product_qty' => $cartItems->sum('qty'),
+                        'payment_method' => 'paypal',
+                        'payment_status' => 'paid',
+                        'total_amount' => $totalCartItemAmount + $shippingRate->rate,
+                        'address' => $address->address,
+                        'shipping_method' => $shippingMethod->name,
+                        'shipping_fee' => $shippingRate->rate,
+                        'coupon' => session('coupon') ? session('coupon')['code'] : null,
+                        'status' => 'pending',
+                    ]);
+
+                    $cartItems->each(function ($item) use ($order, &$productsByStore) {
+                        $orderItem = OrderItem::create([
+                            'order_id' => $order->id,
+                            'product_id' => $item->product_id,
+                            'store_id' => $item->store_id,
+                            'qty' => $item->qty,
+                            'attributes' => $item->attributes,
+                            'unit_price' => $item->unit_price,
+                            'total_price' => $item->total_price,
+                        ]);
+
+                        $productsByStore[$item->store_id][] = $orderItem;
+                    });
+
+                    Transaction::create([
+                        'order_id' => $order->id,
+                        'transaction_id' => null,
+                        'payment_method' => 'paypal',
+                        'amount' => $totalCartItemAmount + $shippingRate->rate,
+                    ]);
+
+                    if (session('coupon')) {
+                        session()->forget('coupon');
+                    }
+
+                    $cartItems->each(function ($item) {
+                        $item->destroy($item->id);
+                    });
+
+                    $placedOrder = Order::with(['user:id,name', 'orderItems.product:id,image,name'])->find($order->id);
+
+                    Mail::to($address->email)->queue(new OrderPlacedSuccessfullyEmail($address, $placedOrder));
+
+                    $admins = User::where('role', 'admin')
+                        ->permission(['orders.edit'])
+                        ->get();
+
+                    $admins->each(function ($admin) use ($address, $placedOrder) {
+                        Mail::to($admin->email)->queue(new NewOrderPlacedEmail($admin, $address, $placedOrder));
+                    });
+
+                    foreach ($productsByStore as $storeId => $orderItems) {
+                        $store = Store::with('seller:id,name')->find($storeId);
+
+                        Mail::to($store->contact_email)->queue(new SellerNewOrderPlacedEmail($store, $address, $orderItems));
+                    }
+                });
+
+                return to_route('home')->with('success', 'Your order placed is successfully.');
+            } else {
+
+                return to_route('payments.paypal.cancel');
+            }
+
+        } catch (Exception $e) {
+            Log::error('PayPal API Error: ' . $e->getMessage());
+
+            return back()->with("error", "Something went wrong!");
         }
-
-        return to_route('payments.paypal.cancel');
     }
 
-    public function paypalCancel(Request $request)
+    public function paypalCancel(Request $request): RedirectResponse
     {
-        return to_route('payments.index')->with('error', 'Something went wrong!');
+        return to_route('payments')->with('error', 'Something went wrong!');
     }
 }
--- resources/js/Pages/E-commerce/Payments/Partials/Paypal.vue
+++ resources/js/Pages/E-commerce/Payments/Partials/Paypal.vue
@@ -1,16 +1,14 @@
 <script setup>
-import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
   <div class="flex items-center justify-center p-16">
-    <Link
-      as="button"
+    <a
       :href="route('payments.paypal.pay')"
       class="px-5 py-3.5 text-white rounded-md bg-[#e6ba2a] hover:bg-[#eeca51] duration-200 font-semibold text-sm"
     >
       <i class="fa-brands fa-paypal"></i>
       Pay with Paypal
-    </Link>
+    </a>
   </div>
 </template>
